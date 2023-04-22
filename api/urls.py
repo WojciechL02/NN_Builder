cd7e1c8e83555e0346ce29472809adf93cbab000
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-04-22 23:59:53 +0200

    CreateNetView implemented

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import main, RegistrationView, LoginView
+from .views import main, RegistrationView, LoginView, CreateNetView
 
 
 urlpatterns = [
     path('', main),
     path('sign-up', RegistrationView.as_view()),
-    path('login', LoginView.as_view())
+    path('login', LoginView.as_view()),
+    path('create', CreateNetView.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -2,7 +2,10 @@ from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser
 from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.contrib.auth import authenticate, login
 from .serializers import UserSerializer
 
@@ -12,6 +15,9 @@ def main(request):
 
 
 class RegistrationView(APIView):
+    authentication_classes = []
+    permission_classes = (AllowAny,)
+
     def post(self, request, format=None):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
@@ -25,6 +31,9 @@ class RegistrationView(APIView):
 
 
 class LoginView(APIView):
+    authentication_classes = []
+    permission_classes = (AllowAny,)
+
     def post(self, request):
         username = request.data.get('username')
         password = request.data.get('password')
@@ -37,4 +46,23 @@ class LoginView(APIView):
 
 
 class CreateNetView(APIView):
-    pass
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    parser_classes = [MultiPartParser]
+
+    def get(self, request, format=None):
+        if request.user.is_authenticated:
+            content = {'user': request.user.username}
+            return Response(content)
+        else:
+            return Response({'error': 'Authentication error'}, status=status.HTTP_401_UNAUTHORIZED)
+
+    def post(self, request, format=None):
+        print("POST")
+        if request.user.is_authenticated:
+            print(request.data)
+            file = request.data['file']
+            print(file)
+            return Response({}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Authentication error'}, status=status.HTTP_401_UNAUTHORIZED)
