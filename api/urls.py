9ca98bdfa543e175e1eeb3bdbe54f2eb6e1d6f75
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-05-02 23:17:58 +0200

    logout view implemented

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import main, RegistrationView, LoginView, CreateNetView
+from .views import main, RegistrationView, LoginView, LogoutView, CreateNetView
 
 
 urlpatterns = [
     path('', main),
     path('sign-up', RegistrationView.as_view()),
     path('login', LoginView.as_view()),
+    path('logout', LogoutView.as_view()),
     path('create', CreateNetView.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -8,6 +8,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.contrib.auth import authenticate, login
 from .serializers import UserSerializer
+from .forms import NetParamsForm
 
 
 def main(request):
@@ -34,7 +35,7 @@ class LoginView(APIView):
     authentication_classes = []
     permission_classes = (AllowAny,)
 
-    def post(self, request):
+    def post(self, request, format=None):
         username = request.data.get('username')
         password = request.data.get('password')
         user = authenticate(request, username=username, password=password)
@@ -42,7 +43,18 @@ class LoginView(APIView):
             login(request, user)
             token, created = Token.objects.get_or_create(user=user)
             return Response({'token': token.key}, status=status.HTTP_200_OK)
-        return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response({'error': 'Unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class LogoutView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, format=None):
+        if request.user.is_authenticated:
+            request.user.auth_token.delete()
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        return Response({'error': 'Authentication error'}, status=status.HTTP_401_UNAUTHORIZED)
 
 
 class CreateNetView(APIView):
@@ -54,15 +66,15 @@ class CreateNetView(APIView):
         if request.user.is_authenticated:
             content = {'user': request.user.username}
             return Response(content)
-        else:
-            return Response({'error': 'Authentication error'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        return Response({'error': 'Authentication error'}, status=status.HTTP_401_UNAUTHORIZED)
 
     def post(self, request, format=None):
-        print("POST")
         if request.user.is_authenticated:
-            print(request.data)
-            file = request.data['file']
-            print(file)
-            return Response({}, status=status.HTTP_200_OK)
-        else:
-            return Response({'error': 'Authentication error'}, status=status.HTTP_401_UNAUTHORIZED)
+            form = NetParamsForm(request.POST, request.FILES)
+            if form.is_valid():
+                # handle_uploaded_file(request.FILES['file'])
+                print(form)
+                return Response({}, status=status.HTTP_200_OK)
+
+        return Response({'error': 'Authentication error'}, status=status.HTTP_401_UNAUTHORIZED)
