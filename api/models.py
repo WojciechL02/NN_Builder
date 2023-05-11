07bc4fe958af82e8cd31dc4370bcea8c9d6b2610
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-05-11 12:16:42 +0200

    net params model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class NetParamsModel(models.Model):
+    # username = models.CharField(max_length=50)
+    loss = models.CharField(max_length=50)
+    optimizer = models.CharField(max_length=50)
+    lr = models.FloatField()
+    epochs = models.PositiveIntegerField()
+    batch = models.PositiveIntegerField()
+    layers = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
