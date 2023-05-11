3304024341cc530a862024c023ef8b5ddcec3ce2
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-05-11 12:20:23 +0200

    chart template

---
+++ frontend/src/components/Chart.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { useTheme } from "@mui/material/styles";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Label,
+  ResponsiveContainer,
+} from "recharts";
+import Title from "./Title";
+
+
+export default function Chart({ responseData }) {
+  const theme = useTheme();
+
+  const title = responseData.title;
+  const data = responseData.map((item) => ({
+    epoch: item.epoch,
+    value: item.value,
+  }));
+
+  return (
+    <React.Fragment>
+      <Title>{title}</Title>
+      <ResponsiveContainer>
+        <LineChart
+          data={data}
+          margin={{
+            top: 16,
+            right: 16,
+            bottom: 0,
+            left: 24,
+          }}
+        >
+          <XAxis
+            dataKey="epoch"
+            stroke={theme.palette.text.secondary}
+            style={theme.typography.body2}
+          />
+          <YAxis
+            stroke={theme.palette.text.secondary}
+            style={theme.typography.body2}
+          >
+            <Label
+              angle={270}
+              position="left"
+              style={{
+                textAnchor: "middle",
+                fill: theme.palette.text.primary,
+                ...theme.typography.body1,
+              }}
+            >
+              Value
+            </Label>
+          </YAxis>
+          <Line
+            isAnimationActive={false}
+            type="monotone"
+            dataKey="amount"
+            stroke={theme.palette.primary.main}
+            dot={false}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </React.Fragment>
+  );
+}
