66848be06437e9a66c00ea8c7d9845f450bc361d
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-05-12 18:44:36 +0200

    class and regr possible

--- training/train.py
+++ training/train.py
@@ -1,6 +1,6 @@
 
 
-def train_epoch(model, train_loader, criterion, optimizer):
+def train_epoch(model, train_loader, criterion, optimizer, task):
     model.train()
     total_loss = 0.
     correct = 0
@@ -10,14 +10,15 @@ def train_epoch(model, train_loader, criterion, optimizer):
         optimizer.zero_grad()
         logits = model(data)
 
-        pred = logits.argmax(dim=1, keepdim=True)
+        if task == 'C':
+            pred = logits.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
         loss = criterion(logits, target)
         loss.backward()
         optimizer.step()
 
         total_loss += loss.item()
-        correct += pred.eq(target.view_as(pred)).sum().item()
         n_samples += len(data)
 
     epoch_loss = total_loss / len(train_loader)
