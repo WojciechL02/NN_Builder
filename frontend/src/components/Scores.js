83f500c1de0fc244bc5d9484f6dfa8c66ce323fe
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-05-12 18:42:05 +0200

    scores table implemented

--- frontend/src/components/Scores.js
+++ frontend/src/components/Scores.js
@@ -4,22 +4,16 @@ import Typography from "@mui/material/Typography";
 import Title from "./Title";
 
 
-export default function Scores() {
+export default function Scores({ responseData }) {
+
     return (
         <React.Fragment>
             <Title>Test scores</Title>
-            <Typography color="text" variant="h6">
-                Accuracy:
-            </Typography>
-            <Typography color="text" variant="h6">
-                Precision:
-            </Typography>
-            <Typography color="text" variant="h6">
-                Recall:
-            </Typography>
-            <Typography color="text" variant="h6">
-                F1-score:
-            </Typography>
+            {Object.entries(responseData).map(([key, value]) => (
+                <Typography key={key} color="text" variant="h6">
+                    {key}: {value}
+                </Typography>
+            ))}
         </React.Fragment>
     );
 }
