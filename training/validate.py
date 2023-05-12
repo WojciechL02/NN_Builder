e45acef6bee68c4317ded0d535f57ef96306c30f
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-05-12 18:44:52 +0200

    use of metrics

--- training/validate.py
+++ training/validate.py
@@ -1,22 +1,22 @@
 import torch
 
 
-def validate_model(model, loader, criterion):
+def validate_model(model, loader, criterion, task, metric_functions):
     model.eval()
     total_loss = 0.
-    correct = 0
-    n_samples = 0
+    metrics = {}
 
     with torch.no_grad():
         for data, target in loader:
             logits = model(data)
 
             total_loss += criterion(logits, target).item()
-            pred = logits.argmax(dim=1, keepdim=True)
 
-            correct += pred.eq(target.view_as(pred)).sum().item()
-            n_samples += len(data)
+            if task == 'C':
+                pred = logits.argmax(dim=1, keepdim=True)
+                for name, metric in metric_functions:
+                    metrics[name] += metric(target, pred)
 
+    metrics = {k: v / len(loader) for k, v in metrics.items()}
     loss = total_loss / len(loader)
-    accuracy = correct / n_samples
-    return loss, accuracy
+    return loss, metrics
