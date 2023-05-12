c682f1ad12647cd0394d1a17f2eb54ce61c5e83f
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-05-12 18:40:49 +0200

    form updated

--- api/forms.py
+++ api/forms.py
@@ -7,9 +7,11 @@ class UploadFileForm(forms.Form):
 
 
 class NetParamsForm(forms.Form):
+    training_size = forms.FloatField(min_value=0., max_value=1.)
     loss = forms.IntegerField(min_value=0)
     optimizer = forms.IntegerField(min_value=0)
     lr = forms.FloatField(min_value=0.)
+    wd = forms.FloatField(min_value=0.)
     epochs = forms.IntegerField(min_value=1)
     batch = forms.IntegerField(min_value=1)
     layers = forms.CharField()
