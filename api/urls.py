25ad000aab29fe6dafd0808636c4ba9d4d915481
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-05-14 18:06:25 +0200

    cleanup

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import main, RegistrationView, LoginView, LogoutView, UploadFileView, CreateNetView, TrainNetView
+from .views import RegistrationView, LoginView, LogoutView, UploadFileView, CreateNetView, TrainNetView
 
 
 urlpatterns = [
-    path('', main),
     path('sign-up', RegistrationView.as_view()),
     path('login', LoginView.as_view()),
     path('logout', LogoutView.as_view()),
