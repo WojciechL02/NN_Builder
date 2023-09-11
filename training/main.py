37c44d5e273ec4b7bb5767199479ba225502d27b
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-09-11 20:47:20 +0200

    clf and regr handled differently

--- training/main.py
+++ training/main.py
@@ -1,13 +1,12 @@
 from .model import Network
-from .train import train_epoch
-from .validate import validate_model
+from .train_clf import train_classif
+from .train_regr import train_regr
+from .validate import validate_classif, validate_regr
 from .dataset import CSVDataset
 from .utils import get_criterion, get_optimizer
 
 import torch
 from torch.utils.data import DataLoader, random_split
-from sklearn.metrics import recall_score, precision_score, f1_score, accuracy_score
-from copy import deepcopy
 
 
 def train_network(params: dict) -> dict:
@@ -35,51 +34,26 @@ def train_network(params: dict) -> dict:
     criterion = get_criterion(params['loss'])
     optimizer = get_optimizer(params['optimizer'], model.parameters(), params['lr'], params['wd'])
 
-    metric_functions = {
-        'Accuracy': accuracy_score,
-        'Recall': recall_score,
-        'Precision': precision_score,
-        'F1-score': f1_score
-    }
-
-    best_score = 0. if TASK == 'C' else float('inf')
-    best_state = deepcopy(model.state_dict())
-    train_loss = []
-    train_acc = []
-    validation_loss = []
-    validation_acc = []
-    for epoch in range(1, EPOCHS + 1):
-        loss, acc = train_epoch(model, train_loader, criterion, optimizer, TASK)
-        val_loss, metrics = validate_model(model, val_loader, criterion, TASK, metric_functions)
-        val_acc = metrics['accuracy']
-
-        train_loss.append(loss)
-        train_acc.append(acc)
-        validation_loss.append(val_loss)
-        validation_acc.append(val_acc)
-
-        if TASK == 'C':
-            if val_acc > best_score:
-                best_score = val_acc
-                best_state = model.state_dict()
-        else:
-            if val_loss < best_score:
-                best_score = val_loss
-                best_state = deepcopy(model.state_dict())
-
-    model.load_state_dict(best_state)
-    test_loss, test_metrics = validate_model(model, test_loader, criterion, TASK, metric_functions)
-    torch.save(best_state, f"././nn_models/{params['username']}.pth")
-
     if TASK == 'C':
-        test_metrics['Loss'] = test_loss
+        best_state, train_loss, train_metric, val_loss, val_metric = train_classif(model, train_loader, val_loader, criterion, optimizer, EPOCHS)
+
+        model.load_state_dict(best_state)
+        test_loss, test_metrics = validate_classif(model, test_loader, criterion)
+        torch.save(best_state, f"././nn_models/{params['username']}.pth")
+
+    elif TASK == 'R':
+        best_state, train_loss, train_metric, val_loss, val_metric = train_regr(model, train_loader, val_loader, criterion, optimizer, EPOCHS)
+
+        model.load_state_dict(best_state)
+        test_loss, test_metrics = validate_regr(model, test_loader, criterion)
+        torch.save(best_state, f"././nn_models/{params['username']}.pth")
 
     results = {
         'task': TASK,
-        'test': test_metrics,
+        'test_metrics': test_metrics,
         'train_loss': train_loss,
-        'train_accuracy': train_acc,
-        'val_loss': validation_loss,
-        'val_accuracy': validation_acc
+        'train_metric': train_metric,
+        'val_loss': val_loss,
+        'val_metric': val_metric
     }
     return results
