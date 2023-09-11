986f9fd24115ca9a0f9ed767dc8a50229831c66d
Wojciech Lapacz <wojciech.lapacz02@gmail.com>
2023-09-11 20:49:45 +0200

    get cookie method implemented

--- frontend/src/components/utils.js
+++ frontend/src/components/utils.js
@@ -1,21 +1,7 @@
 
 
-function getCookie(name) {
+export function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(";").shift();
-}
-
-
-function handleLogout() {
-    const requestOptions = {
-        method: "POST",
-        headers: {
-            'X-CSRFToken': getCookie("csrftoken"),
-        },
-    }
-    fetch("/api/logout", requestOptions)
-    .then(() => {
-        navigate("/");
-    });
 }
